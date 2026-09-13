751baabb628f9174c568bdd099e019af60a13b82
Sai Prasad <bsaiprasad13@gmail.com>
2026-09-13 23:02:23 +0530

    Fix unresolved reference to util in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.compose.compiler)
@@ -14,10 +17,10 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        val localProperties = java.util.Properties()
+        val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
         if (localPropertiesFile.exists()) {
-            localProperties.load(java.io.FileInputStream(localPropertiesFile))
+            localProperties.load(FileInputStream(localPropertiesFile))
         }
         val userName = localProperties.getProperty("USER_NAME", "There")
         buildConfigField("String", "USER_NAME", "\"$userName\"")
